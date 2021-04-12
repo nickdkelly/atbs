d1f1bc52c3c4e4f7282b96285cd6b7e89bad77fd
Nick Kelly <nickdkelly@me.com>
2021-04-12 13:20:56 +0100

    more loop exercises

---
+++ yourName.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Annoying name program
+name = ''
+while name != ('your name'):
+    name = input ('please type your name ')
+print('thank you!')    
---
+++ yourName2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+while True:
+    print('Please type your name: ')
+    name = input()
+    # when condition is met
+    if name == 'your name':
+        # break out of the loop, which takes you back to the same indentation level as where the loop starts!
+        break
+print ('thank you!')    
