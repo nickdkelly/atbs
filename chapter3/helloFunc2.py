feb4283126bb3c5202cd6ef753107f08724d368e
Nick Kelly <nickdkelly@me.com>
2021-04-13 19:03:27 +0100

    functions part 1

---
+++ chapter3/helloFunc2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def hello (name): # when an argument is defined as part of the function, it must be passed in when the function gets called 
+    print('Hello, ' + name)
+
+hello('Alice') # like so
+hello('Bob')
